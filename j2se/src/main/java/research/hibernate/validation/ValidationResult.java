89f203f1ac0dc9f189cf0221e0a13978dd4599f1
wdmcygah <wdmcygah@163.com>
2015-01-10 20:34:59 +0800

    Hibernate validation

--- j2se/src/main/java/research/hibernate/validation/ValidationResult.java
+++ j2se/src/main/java/research/hibernate/validation/ValidationResult.java
@@ -9,8 +9,10 @@ import java.util.Map;
  */
 public class ValidationResult {
 
+	//校验结果是否有错
 	private boolean hasErrors;
 	
+	//校验错误信息
 	private Map<String,String> errorMsg;
 
 	public boolean isHasErrors() {
